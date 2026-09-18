a2b075f08658b68abbc049fb02ac7f76fd60d3d2
GIRISH-750 <girishkadam2634@gmail.com>
2026-09-18 17:04:55 +0000

    Complete Question 11

---
+++ Question-11/a/question_11a.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main() {
+    int fd1, fd2;
+
+    fd1 = open("testfile.txt", O_WRONLY | O_APPEND);
+
+    if (fd1 == -1) {
+        perror("open");
+        return 1;
+    }
+
+    fd2 = dup(fd1);
+
+    if (fd2 == -1) {
+        perror("dup");
+        close(fd1);
+        return 1;
+    }
+
+    write(fd1, "Written using fd1\n", 18);
+    write(fd2, "Written using fd2\n", 18);
+
+    close(fd1);
+    close(fd2);
+
+    return 0;
+}
