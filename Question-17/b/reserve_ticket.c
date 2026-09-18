27309b5bf708c62a50bef7c903cce3eafbc155be
GIRISH-750 <girishkadam2634@gmail.com>
2026-09-18 17:17:33 +0000

    Complete Question 17

---
+++ Question-17/b/reserve_ticket.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main() {
+    int fd;
+    int ticket;
+    struct flock lock;
+
+    fd = open("../ticket.txt", O_RDWR);
+
+    if (fd == -1) {
+        perror("open");
+        return 1;
+    }
+
+    lock.l_type = F_WRLCK;
+    lock.l_whence = SEEK_SET;
+    lock.l_start = 0;
+    lock.l_len = 0;
+    lock.l_pid = getpid();
+
+    if (fcntl(fd, F_SETLKW, &lock) == -1) {
+        perror("fcntl");
+        close(fd);
+        return 1;
+    }
+
+    read(fd, &ticket, sizeof(ticket));
+
+    ticket++;
+
+    printf("New ticket number: %d\n", ticket);
+
+    lseek(fd, 0, SEEK_SET);
+    write(fd, &ticket, sizeof(ticket));
+
+    lock.l_type = F_UNLCK;
+    fcntl(fd, F_SETLK, &lock);
+
+    close(fd);
+
+    return 0;
+}
