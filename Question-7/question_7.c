5d4d5c7b0f1456f4e7b4388a5ae810553713877e
GIRISH-750 <girishkadam2634@gmail.com>
2026-09-18 16:43:56 +0000

    Complete Question 7

---
+++ Question-7/question_7.c
@@ -0,0 +1,33 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+    int fd1, fd2;
+    char buffer[100];
+    int n;
+
+    fd1 = open("file1.txt", O_RDONLY);
+
+    if (fd1 == -1) {
+        perror("file1");
+        return 1;
+    }
+
+    fd2 = open("file2.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    if (fd2 == -1) {
+        perror("file2");
+        close(fd1);
+        return 1;
+    }
+
+    while ((n = read(fd1, buffer, sizeof(buffer))) > 0) {
+        write(fd2, buffer, n);
+    }
+
+    close(fd1);
+    close(fd2);
+
+    return 0;
+}
