be3fadebc52d34a06c13ff5cb50357e5543dd268
GIRISH-750 <girishkadam2634@gmail.com>
2026-09-18 17:08:44 +0000

    Complete Question 13

---
+++ Question-13/question_13.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/select.h>
+
+int main() {
+    fd_set readfds;
+    struct timeval timeout;
+    int result;
+
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    timeout.tv_sec = 10;
+    timeout.tv_usec = 0;
+
+    printf("Waiting for input for 10 seconds...\n");
+
+    result = select(STDIN_FILENO + 1, &readfds, NULL, NULL, &timeout);
+
+    if (result == -1) {
+        perror("select");
+    }
+    else if (result == 0) {
+        printf("No data available within 10 seconds.\n");
+    }
+    else {
+        if (FD_ISSET(STDIN_FILENO, &readfds)) {
+            printf("Data is available on STDIN.\n");
+        }
+    }
+
+    return 0;
+}
