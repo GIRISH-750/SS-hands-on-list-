e86615f72d064b8253e16a6c4cc00f7966f8d360
GIRISH-750 <girishkadam2634@gmail.com>
2026-09-21 20:21:31 +0000

    Complete Question 23

---
+++ Question-23/question_23.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main()
+{
+    pid_t pid;
+
+    pid = fork();
+
+    if (pid < 0)
+    {
+        perror("fork");
+        return 1;
+    }
+    else if (pid == 0)
+    {
+        printf("Child process started\n");
+        printf("Child PID: %d\n", getpid());
+        printf("Initial Parent PID: %d\n", getppid());
+
+        sleep(10);
+
+        printf("After parent exits:\n");
+        printf("Child PID: %d\n", getpid());
+        printf("New Parent PID: %d\n", getppid());
+    }
+    else
+    {
+        printf("Parent process\n");
+        printf("Parent PID: %d\n", getpid());
+        printf("Child PID: %d\n", pid);
+
+        sleep(2);
+
+        printf("Parent exiting...\n");
+        exit(0);
+    }
+
+    return 0;
+}
